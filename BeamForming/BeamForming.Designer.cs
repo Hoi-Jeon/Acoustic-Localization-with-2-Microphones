d461baa90c8e971165f1768d0c9a46da5b2d04b3
Hoi-Jeon <66248623+Hoi-Jeon@users.noreply.github.com>
2020-06-13 13:18:52 +0200

    Add files via upload

---
+++ BeamForming/BeamForming.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace BeamForming
+{
+    partial class BeamForming
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSTOP = new System.Windows.Forms.Button();
+            this.btnSTART = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssLBLstatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsLBLSpacer1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lbNBytes = new System.Windows.Forms.ToolStripStatusLabel();
+            this.srPort = new System.IO.Ports.SerialPort(this.components);
+            this.cboVideo = new System.Windows.Forms.ComboBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblVideoPort = new System.Windows.Forms.Label();
+            this.chartBFM = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblSerialPort = new System.Windows.Forms.Label();
+            this.tbFreq = new System.Windows.Forms.TextBox();
+            this.lblFrq = new System.Windows.Forms.Label();
+            this.lblMicSpacing = new System.Windows.Forms.Label();
+            this.tbMicSpacing = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBFM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(5, 28);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(70, 21);
+            this.cboPorts.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(5, 53);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(70, 25);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(5, 81);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(70, 25);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSTOP
+            // 
+            this.btnSTOP.Enabled = false;
+            this.btnSTOP.Location = new System.Drawing.Point(5, 197);
+            this.btnSTOP.Name = "btnSTOP";
+            this.btnSTOP.Size = new System.Drawing.Size(70, 25);
+            this.btnSTOP.TabIndex = 12;
+            this.btnSTOP.Text = "STOP";
+            this.btnSTOP.UseVisualStyleBackColor = true;
+            this.btnSTOP.Click += new System.EventHandler(this.btnSTOP_Click);
+            // 
+            // btnSTART
+            // 
+            this.btnSTART.Enabled = false;
+            this.btnSTART.Location = new System.Drawing.Point(5, 169);
+            this.btnSTART.Name = "btnSTART";
+            this.btnSTART.Size = new System.Drawing.Size(70, 25);
+            this.btnSTART.TabIndex = 14;
+            this.btnSTART.Text = "START";
+            this.btnSTART.UseVisualStyleBackColor = true;
+            this.btnSTART.Click += new System.EventHandler(this.btnSTART_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLBLstatus,
+            this.tsLBLSpacer1,
+            this.toolStripStatusLabel1,
+            this.lbNBytes});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 369);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(617, 22);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssLBLstatus
+            // 
+            this.tssLBLstatus.Name = "tssLBLstatus";
+            this.tssLBLstatus.Size = new System.Drawing.Size(79, 17);
+            this.tssLBLstatus.Text = "Disconnected";
+            // 
+            // tsLBLSpacer1
+            // 
+            this.tsLBLSpacer1.Name = "tsLBLSpacer1";
+            this.tsLBLSpacer1.Size = new System.Drawing.Size(356, 17);
+            this.tsLBLSpacer1.Spring = true;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(142, 17);
+            this.toolStripStatusLabel1.Text = "Number of data in buffer:";
+            // 
+            // lbNBytes
+            // 
+            this.lbNBytes.Name = "lbNBytes";
+            this.lbNBytes.Size = new System.Drawing.Size(25, 17);
+            this.lbNBytes.Text = "000";
+            // 
+            // srPort
+            // 
+            this.srPort.BaudRate = 115200;
+            this.srPort.PortName = "COM3";
+            this.srPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.srPort_DataReceived);
+            // 
+            // cboVideo
+            // 
+            this.cboVideo.FormattingEnabled = true;
+            this.cboVideo.Location = new System.Drawing.Point(5, 144);
+            this.cboVideo.Name = "cboVideo";
+            this.cboVideo.Size = new System.Drawing.Size(70, 21);
+            this.cboVideo.TabIndex = 16;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Location = new System.Drawing.Point(5, 320);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(70, 25);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblVideoPort
+            // 
+            this.lblVideoPort.Location = new System.Drawing.Point(2, 126);
+            this.lblVideoPort.Name = "lblVideoPort";
+            this.lblVideoPort.Size = new System.Drawing.Size(70, 18);
+            this.lblVideoPort.TabIndex = 18;
+            this.lblVideoPort.Text = "Select video:";
+            this.lblVideoPort.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // chartBFM
+            // 
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chartBFM.ChartAreas.Add(chartArea1);
+            this.chartBFM.Location = new System.Drawing.Point(99, 9);
+            this.chartBFM.Name = "chartBFM";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chartBFM.Series.Add(series1);
+            this.chartBFM.Size = new System.Drawing.Size(506, 352);
+            this.chartBFM.TabIndex = 20;
+            this.chartBFM.Text = "chart1";
+            // 
+            // lblSerialPort
+            // 
+            this.lblSerialPort.Location = new System.Drawing.Point(2, 9);
+            this.lblSerialPort.Name = "lblSerialPort";
+            this.lblSerialPort.Size = new System.Drawing.Size(64, 18);
+            this.lblSerialPort.TabIndex = 21;
+            this.lblSerialPort.Text = "Serial port:";
+            this.lblSerialPort.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbFreq
+            // 
+            this.tbFreq.Location = new System.Drawing.Point(5, 257);
+            this.tbFreq.Name = "tbFreq";
+            this.tbFreq.Size = new System.Drawing.Size(70, 20);
+            this.tbFreq.TabIndex = 22;
+            this.tbFreq.Text = "512";
+            // 
+            // lblFrq
+            // 
+            this.lblFrq.Location = new System.Drawing.Point(2, 238);
+            this.lblFrq.Name = "lblFrq";
+            this.lblFrq.Size = new System.Drawing.Size(70, 18);
+            this.lblFrq.TabIndex = 23;
+            this.lblFrq.Text = "Freq [Hz]:";
+            this.lblFrq.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblMicSpacing
+            // 
+            this.lblMicSpacing.Location = new System.Drawing.Point(2, 277);
+            this.lblMicSpacing.Name = "lblMicSpacing";
+            this.lblMicSpacing.Size = new System.Drawing.Size(91, 18);
+            this.lblMicSpacing.TabIndex = 24;
+            this.lblMicSpacing.Text = "Spacing [mm]:";
+            this.lblMicSpacing.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbMicSpacing
+            // 
+            this.tbMicSpacing.Location = new System.Drawing.Point(5, 296);
+            this.tbMicSpacing.Name = "tbMicSpacing";
+            this.tbMicSpacing.Size = new System.Drawing.Size(70, 20);
+            this.tbMicSpacing.TabIndex = 25;
+            this.tbMicSpacing.Text = "150";
+            // 
+            // BeamForming
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 391);
+            this.Controls.Add(this.tbMicSpacing);
+            this.Controls.Add(this.lblMicSpacing);
+            this.Controls.Add(this.lblFrq);
+            this.Controls.Add(this.tbFreq);
+            this.Controls.Add(this.lblSerialPort);
+            this.Controls.Add(this.lblVideoPort);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.cboVideo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSTART);
+            this.Controls.Add(this.btnSTOP);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.chartBFM);
+            this.Name = "BeamForming";
+            this.Text = "BeamForming -Tiva C series TM4C123GXL";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BeamForming_FormClosed);
+            this.Load += new System.EventHandler(this.BeamForming_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBFM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSTOP;
+        private System.Windows.Forms.Button btnSTART;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssLBLstatus;
+        private System.Windows.Forms.ToolStripStatusLabel tsLBLSpacer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lbNBytes;
+        private System.IO.Ports.SerialPort srPort;
+        private System.Windows.Forms.ComboBox cboVideo;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblVideoPort;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBFM;
+        private System.Windows.Forms.Label lblSerialPort;
+        private System.Windows.Forms.TextBox tbFreq;
+        private System.Windows.Forms.Label lblFrq;
+        private System.Windows.Forms.Label lblMicSpacing;
+        private System.Windows.Forms.TextBox tbMicSpacing;
+    }
+}
+
---
+++ BeamForming/BeamForming.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace BeamForming
+{
+    public partial class BeamForming : Form
+    {
+        private const Int32 NFFT = 128;                     ///< Number of FFT
+        private byte[] BufferPhase = new byte[NFFT * 2];    ///< Internal Buffer to buffer FFT signal (1 float = 4 bytes)
+        private float[] Phase = new float[NFFT / 2];        ///< Converted a phase between ADC0 and ADC1
+
+        static byte[] CMD_NO_CMD = { 0x00 };                ///< no command was actually send by serial connection
+        static byte[] CMD_SEND_ADC = { 0x01 };              ///< Start sending ADC (Time domain) signal
+        static byte[] CMD_STOP = { 0x02 };                  ///< Stop sending ADC|FFT signal
+        static byte[] CMD_SEND_FFT = { 0x03 };              ///< Start sending FFT signal
+        static byte[] CMD_SEND_PHASE = { 0x04 };            ///< Start sending a phase between ADC0 and ADC1 signals
+        static byte[] CMD_DISCONNECT = { 0x0D };            ///< Disconnect serail connection to Tiva Board
+        private byte[] CMD_CURRENT = CMD_NO_CMD;            ///< Current CMD
+
+        private Int32 MinByte = 2;                          ///< Minimum numer of bytes to read at one time in serial port
+        private Int32 idxBuffer = 0;                        ///< index of the last read element
+        private Int32 NbytesToRead;                         ///< Numer of bytes to read at one time in serial port
+
+        private Bitmap chartBackImage;                      ///< Backimage of C# chart
+        private Int32 Freq;                                 ///< Beamforming frequency
+        private Int32 IdxFreq;                              ///< Beamforming frequency index
+        private Int32 dFreq = 32;                           ///< fixed in Tiva Launchpad (Fs / Nfft = 2048 / 128)
+        private float MicSpacing;                           ///< Microphone spacing between two microphones
+
+        FilterInfoCollection filterInfoCollection;          ///< Filter Info collection for AForge.Video.DirectShow
+        VideoCaptureDevice videoCaptureDevice;              ///< Video capture device
+
+        public BeamForming()
+        {
+            InitializeComponent();
+        }
+
+        private void BeamForming_Load(object sender, EventArgs e)
+        {
+
+            // Initialize Controls in GUI
+            for (int n = -35; n <= 35; n++)
+            {
+                chartBFM.Series["Series1"].Points.AddXY(n, 1);
+            }
+            chartBFM.Titles.Add("Acoustic localization with two microphones");
+            chartBFM.ChartAreas[0].AxisX.Title = "Angle [degree]";
+            chartBFM.ChartAreas[0].AxisX.Minimum = -35;
+            chartBFM.ChartAreas[0].AxisX.Maximum = 35;
+            chartBFM.ChartAreas[0].AxisX.Interval = 5;
+            chartBFM.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chartBFM.ChartAreas[0].AxisY.Minimum = 0;
+            chartBFM.ChartAreas[0].AxisY.Maximum = 1;
+            chartBFM.ChartAreas[0].AxisY.Interval = 2;
+            chartBFM.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Solid;
+            chartBFM.ChartAreas[0].AxisY.MajorGrid.Interval = 2;
+            chartBFM.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+                        
+            chartBFM.ChartAreas[0].CursorX.LineColor = Color.Red;
+            chartBFM.ChartAreas[0].CursorX.LineWidth = 5;
+            chartBFM.ChartAreas[0].CursorX.LineDashStyle = ChartDashStyle.Solid;
+
+            try
+            {
+                // List available video device
+                filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboVideo.Items.Add(filterInfo.Name);
+                cboVideo.SelectedIndex = 0;
+                videoCaptureDevice = new VideoCaptureDevice();
+
+                // List available serial port names
+                string[] ArrayComPortsNames = SerialPort.GetPortNames();
+                int index = -1;
+                string ComPortName = null;
+                do
+                {
+                    index += 1;
+                    cboPorts.Items.Add(ArrayComPortsNames[index]);
+                }
+                while (!((ArrayComPortsNames[index] == ComPortName) || (index == ArrayComPortsNames.GetUpperBound(0))));
+
+                // Sort all available COM ports
+                Array.Sort(ArrayComPortsNames);
+
+                // Get the first COM port for the text in the ComboBox
+                if (index == ArrayComPortsNames.GetUpperBound(0)) 
+                    ComPortName = ArrayComPortsNames[0];
+
+                // Select the first port name for the text of combobox
+                cboPorts.Text = ArrayComPortsNames[0]; 
+
+                // Set a frequency and Microphone spacing
+                Freq = Convert.ToInt32(tbFreq.Text);
+                IdxFreq = Freq / dFreq; // unit in Hz
+                MicSpacing = Convert.ToSingle(tbMicSpacing.Text)*0.001f; // unit in meter
+            }            
+
+            catch (IndexOutOfRangeException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void btnSTART_Click(object sender, EventArgs e)
+        {
+            // Start Video Capture
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboVideo.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+            // Update the current command
+            CMD_CURRENT = CMD_SEND_PHASE;
+            srPort.Write(CMD_CURRENT, 0, 1);
+
+            // Activate and deactivate buttons
+            btnSTART.Enabled = false;
+            btnSTOP.Enabled = true;
+            btnUpdate.Enabled = true;
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            chartBackImage = (Bitmap)eventArgs.Frame.Clone();
+            BeginInvoke(new EventHandler(UpdateVideos));
+        }
+
+        private void UpdateVideos(object sender, EventArgs e)
+        {
+            if (chartBFM.Images.Count != 0)
+                chartBFM.Images.RemoveAt(0);
+
+            chartBFM.Images.Add(new NamedImage("DynamicImage", chartBackImage));
+            chartBFM.ChartAreas[0].BackImage = chartBFM.Images[0].Name;
+            chartBFM.ChartAreas[0].BackImageWrapMode = ChartImageWrapMode.Scaled;
+        }
+
+
+        private void btnSTOP_Click(object sender, EventArgs e)
+        {
+            // Stop video capture device
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+
+            // Update the current command
+            CMD_CURRENT = CMD_STOP;
+            srPort.Write(CMD_CURRENT, 0, 1);
+
+            // Run out of all remaining received bytess
+            while (srPort.BytesToRead > 0) { }
+
+            // Activate and deactivate buttons
+            btnSTART.Enabled = true;
+            btnSTOP.Enabled = false;
+            btnUpdate.Enabled = false;
+
+            // Clean Serial port
+            srPort.DiscardInBuffer();
+            srPort.DiscardOutBuffer();
+
+            // Reset counter index
+            idxBuffer = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Open Serial port
+            srPort.PortName = Convert.ToString(cboPorts.Text);
+            srPort.ReceivedBytesThreshold = MinByte; // Minumum size of received bytes in Serial port
+            srPort.Open();
+
+            // Run out of all remaining received bytess
+            while (srPort.BytesToRead > 0) { }
+
+            // Update the current command
+            CMD_CURRENT = CMD_STOP;
+            srPort.Write(CMD_CURRENT, 0, 1);
+
+            // Activate and deactivate buttons
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+            btnSTART.Enabled = true;
+            btnSTOP.Enabled = false;
+            btnUpdate.Enabled = false;
+
+            // Change texts in the status bar
+            tssLBLstatus.Text = srPort.PortName;
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            // Update the current command
+            CMD_CURRENT = CMD_STOP;
+            srPort.Write(CMD_CURRENT, 0, 1);
+
+            // Suspend 0.1 second, so the last serial command can be sent to TI Launchpad 100% securely
+            System.Threading.Thread.Sleep(100); 
+
+            // Close Serial port
+            srPort.DiscardInBuffer();
+            srPort.DiscardOutBuffer();
+            srPort.Close();
+
+            // Stop video capture device
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+
+            // Activate and deactivate buttons
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            btnSTART.Enabled = false;
+            btnSTOP.Enabled = false;
+            btnUpdate.Enabled = false;
+
+            // Change texts in the status bar
+            tssLBLstatus.Text = "Disconnected";
+            lbNBytes.Text = "0";
+        }
+
+        private void BeamForming_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Stop video capture device
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+
+            // Close any thread still running in the background
+            Application.ExitThread();
+            Environment.Exit(0);
+        }
+
+        private void srPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Get a number of read data from serial port
+            NbytesToRead = srPort.BytesToRead;
+
+            // Display a number of read data from serial port                
+            this.BeginInvoke(new EventHandler(DisplayNbytesToRead));
+
+            // Create a byte array for the data from serial port
+            byte[] Buffer = new byte[NbytesToRead];
+            srPort.Read(Buffer, 0, NbytesToRead);
+
+            // Get a phase and Calculate an angle at the wished frequency               
+            if (CMD_CURRENT == CMD_SEND_PHASE)
+            {
+                // Copy data in a buffer to the internal buffer
+                for (int n = 0; n < NbytesToRead; n++)
+                {
+                    if (idxBuffer == NFFT * 2)
+                    {
+                        for (int i = 0; i < (NFFT / 2); i++)
+                        {
+                            // Convert 4 bytes into 1 floats
+                            Phase[i] = BitConverter.ToSingle(BufferPhase, 4 * i);
+                        }
+
+                        // Display the read ADC signal to Chart
+                        this.BeginInvoke(new EventHandler(DisplayMovingCursor));
+
+                        // Reset the location of the last read element
+                        idxBuffer = 0;
+                    }
+
+                    // Copy data into a temporary buffer
+                    BufferPhase[idxBuffer] = Buffer[n];
+                    // Increase the index of the last read element 
+                    idxBuffer++;
+                }
+            }
+        }
+
+        private void DisplayMovingCursor(object sender, EventArgs e)
+        {
+            try
+            {
+                // Change a red cursor position with the phase input
+                Double PhaseInDegree = 180 / (Math.PI) * Math.Asin(Phase[IdxFreq] * 343.1f / MicSpacing / Freq / (2 * Math.PI));
+                chartBFM.ChartAreas[0].CursorX.SetCursorPosition(PhaseInDegree);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void DisplayNbytesToRead(object sender, EventArgs e)
+        {
+            try
+            {
+                lbNBytes.Text = NbytesToRead.ToString("000");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Freq = Convert.ToInt32(tbFreq.Text);
+            IdxFreq = Freq / dFreq; // unit in Hz
+            MicSpacing = Convert.ToSingle(tbMicSpacing.Text) * 0.001f; // unit in meter
+        }
+    }
+}
